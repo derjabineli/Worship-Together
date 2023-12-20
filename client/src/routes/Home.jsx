f961b18716d28dd7f206b1e4a2db2c2fcbae0e6c
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2023-12-19 16:00:06 -0800

    home layout

---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import SongList from "../components/SongList";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <SongList />
+    </div>
+  );
+};
+
+export default Home;
