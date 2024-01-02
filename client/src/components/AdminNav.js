01e822a67d8538474009d940c92f6d528e8f333e
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-01-02 15:12:53 -0800

    added Navigation

---
+++ client/src/components/AdminNav.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const AdminNav = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-dark">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a href="/songs/new">
+                <button class="btn btn-primary">New Song</button>
+              </a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default AdminNav;
